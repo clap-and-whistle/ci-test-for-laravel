4827fbaaee2615c1222dd894cdccdd12a4234d0c
Tatsuhiko Kimita <nannbo@crappo.net>
2021-04-15 10:10:41 +0900

    TestCaseのクラス名を「Laravelの提供するTestCase」であることを示すように変更

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,9 +3,9 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+use Tests\LaraTestCase;
 
-class ExampleTest extends TestCase
+class ExampleTest extends LaraTestCase
 {
     /**
      * A basic test example.
