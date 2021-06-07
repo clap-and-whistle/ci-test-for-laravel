d9ee076f0421cfac7c1480861c378e0f1bd4e704
Tatsuhiko Kimita <nannbo@crappo.net>
2021-06-07 13:37:48 +0900

    #9: ユースケース「アカウント作成の申請をする」の代替系列用例外クラスを用意した

--- BizLogics/UseCase/UserOperation/CreateAccount/CreateAccountUseCase.php
+++ BizLogics/UseCase/UserOperation/CreateAccount/CreateAccountUseCase.php
@@ -4,13 +4,29 @@ declare(strict_types=1);
 namespace Bizlogics\UseCase\UserOperation\CreateAccount;
 
 
+use Bizlogics\Aggregate\User\Exception\PasswordSizeTooShortException;
+use Bizlogics\Aggregate\User\Exception\PasswordTypeCompositionInvalidException;
+use Bizlogics\UseCase\UserOperation\CreateAccount\Exception\ApplyingException;
+use Bizlogics\UseCase\UserOperation\CreateAccount\Exception\EmailAlreadyUsedException;
+
 final class CreateAccountUseCase
 {
     public function execute(): Result
     {
         $result = new Result();
+        try {
+            $userId = 0;
+//            $result->setFailure(new \RuntimeException('hoge'), '');
+        } catch (ApplyingException $e) {
+            $result->setFailure(new $e, '仮: '. get_class($e));
+        } catch (EmailAlreadyUsedException $e) {
+            $result->setFailure(new $e, '仮: '. get_class($e));
+        } catch (PasswordSizeTooShortException $e) {
+            $result->setFailure(new $e, '仮: '. get_class($e));
+        } catch (PasswordTypeCompositionInvalidException $e) {
+            $result->setFailure(new $e, '仮: '. get_class($e));
+        }
 
-        $result->setFailure(new \RuntimeException('hoge'), '');
         return $result;
     }
 }
---
+++ BizLogics/UseCase/UserOperation/CreateAccount/Exception/ApplyingException.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+namespace Bizlogics\UseCase\UserOperation\CreateAccount\Exception;
+
+use RuntimeException;
+
+final class ApplyingException extends RuntimeException
+{
+
+}
