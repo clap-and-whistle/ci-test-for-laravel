5c310aa41fa6fcf9dda893d23903b2c1c9501566
Tatsuhiko Kimita <nannbo@crappo.net>
2021-06-16 13:59:21 +0900

    #13: ユニットテスト用のコードを書いた

--- tests/Unit/UseCase/UserOperation/Login/LoginUseCaseTest.php
+++ tests/Unit/UseCase/UserOperation/Login/LoginUseCaseTest.php
@@ -3,8 +3,12 @@ declare(strict_types=1);
 
 namespace Tests\Unit\UseCase\UserOperation\Login;
 
+use App\Infrastructure\AggregateRepository\User\Exception\NotExistException;
+use App\Infrastructure\AggregateRepository\User\Exception\PasswordIsNotMatchException;
 use App\Infrastructure\AggregateRepository\User\ForTestUserAggregateRepository;
 use Bizlogics\Aggregate\UserAggregateRepositoryInterface;
+use Bizlogics\UseCase\UserOperation\Login\AuthenticatableInterface;
+use Bizlogics\UseCase\UserOperation\Login\LoginUseCase;
 use Tests\BaseTestCase;
 
 class LoginUseCaseTest extends BaseTestCase
@@ -22,8 +26,20 @@ class LoginUseCaseTest extends BaseTestCase
      */
     public function システムへログインする_基本系列(): void
     {
+        $useCase = new LoginUseCase($this->userRepos);
+        $result = $useCase->execute(
+            ForTestUserAggregateRepository::例外用_ユーザID_1_申請中メールアドレス,
+            ForTestUserAggregateRepository::テスト用Password
+        );
+        if (!$result->isSuccess()) {
+            var_dump([__METHOD__ => ['eMessage' => $result->eMessage()]]);
+        }
+
         // 1. result->isSuccess() が true であること
+        self::assertTrue($result->isSuccess());
+
         // 2. result->authObj() がオブジェクトを返すこと
+        self::assertInstanceOf(AuthenticatableInterface::class, $result->authObj());
     }
 
     /**
@@ -32,8 +48,18 @@ class LoginUseCaseTest extends BaseTestCase
      */
     public function システムへログインする_代替系列_入力されたメールアドレスの一致する既存の一般ユーザアカウントが無い場合(): void
     {
+        $useCase = new LoginUseCase($this->userRepos);
+        $result = $useCase->execute(
+            ForTestUserAggregateRepository::システムログイン時例外用_使用されていないメールアドレス,
+            ForTestUserAggregateRepository::テスト用Password
+        );
+
         // 1. result->isSuccess() が false であること
+        self::assertFalse($result->isSuccess());
+
         // 2. result->exception() が NotExistException を返すこと
+        $exception = $result->exception();
+        self::assertSame(NotExistException::class, $exception ? get_class($exception) : "");
     }
 
     /**
@@ -42,8 +68,18 @@ class LoginUseCaseTest extends BaseTestCase
      */
     public function システムへログインする_代替系列_入力されたパスワードが、対象一般ユーザアカウントのパスワードと一致しない場合(): void
     {
+        $useCase = new LoginUseCase($this->userRepos);
+        $result = $useCase->execute(
+            ForTestUserAggregateRepository::例外用_ユーザID_2_既に使用されているメールアドレス,
+            ForTestUserAggregateRepository::テスト用の誤ったPassword
+        );
+
         // 1. result->isSuccess() が false であること
+        self::assertFalse($result->isSuccess());
+
         // 2. result->exception() が PasswordIsNotMatchException を返すこと
+        $exception = $result->exception();
+        self::assertSame(PasswordIsNotMatchException::class, $exception ? get_class($exception) : "");
     }
 
 }
