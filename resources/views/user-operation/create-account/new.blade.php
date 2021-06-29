a44c3f0c8e3b33bbbc423329e476feedf5364d54
Tatsuhiko Kimita <nannbo@crappo.net>
2021-06-29 10:49:14 +0900

    #11: bladeファイルへ微修正

--- resources/views/user-operation/create-account/new.blade.php
+++ resources/views/user-operation/create-account/new.blade.php
@@ -53,8 +53,8 @@
                                     <input type="email" id="email" name="email" value="" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                 </div>
                                 <div class="relative mb-4">
-                                    <label for="message" class="leading-7 text-sm text-gray-600">password</label>
-                                    <input type="password" id="password" name="password" value="*{password}" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                    <label for="password" class="leading-7 text-sm text-gray-600">password</label>
+                                    <input type="password" id="password" name="password" value="" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                 </div>
                                 <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規アカウントを作成する</button>
                             </div>
